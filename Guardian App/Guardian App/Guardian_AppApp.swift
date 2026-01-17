ebe7f479839e0ec11b7b13c80a33eb8435cfa872
Benjamin Adovasio <benjamin@adovasio.com>
2026-01-17 15:49:34 -0500

    Initial native iOS project

---
+++ Guardian App/Guardian App/Guardian_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guardian_AppApp.swift
+//  Guardian App
+//
+//  Created by Benjamin on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct Guardian_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
