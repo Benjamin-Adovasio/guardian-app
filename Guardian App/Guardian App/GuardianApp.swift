636f8304b788c396e86fd211e1735b094fafd20e
Benjamin Adovasio <benjamin@adovasio.com>
2026-01-17 16:56:51 -0500

    Fix iOS navigation using NavigationStack

---
+++ Guardian App/Guardian App/GuardianApp.swift
@@ -0,0 +1,18 @@
+//
+//  GuardianApp.swift
+//  Guardian App
+//
+//  Created by Benjamin on 1/17/26.
+//
+
+
+import SwiftUI
+
+@main
+struct GuardianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
