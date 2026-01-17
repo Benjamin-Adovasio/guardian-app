8abe417e50c656e6e05041925360eadb32ddbdf6
Benjamin Adovasio <benjamin@adovasio.com>
2026-01-17 17:28:38 -0500

    Added First Aid page

--- Guardian App/Guardian App/Views/FirstAidView.swift
+++ Guardian App/Guardian App/Views/FirstAidView.swift
@@ -2,9 +2,23 @@ import SwiftUI
 
 struct FirstAidView: View {
     var body: some View {
-        Text("First Aid")
-            .font(.largeTitle)
+        ScrollView {
+            VStack(spacing: 16) {
+
+                GroupBox("CPR") {
+                    Text("Check responsiveness\nCall 911\nBegin compressions")
+                }
+
+                GroupBox("Bleeding") {
+                    Text("Apply firm pressure\nElevate if possible")
+                }
+
+                GroupBox("Choking") {
+                    Text("Encourage coughing\nPerform abdominal thrusts if needed")
+                }
+            }
             .padding()
+        }
+        .navigationTitle("First Aid")
     }
 }
-
