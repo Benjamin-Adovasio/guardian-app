5ab8f5b1a6b65e31c9f9e0610bf873844e7bcb0a
Benjamin Adovasio <benjamin@adovasio.com>
2026-01-19 15:52:33 -0500

    Changed AED list label

--- Guardian App/Guardian App/Views/AEDLocationsView.swift
+++ Guardian App/Guardian App/Views/AEDLocationsView.swift
@@ -48,14 +48,6 @@ struct AEDLocationsView: View {
                     span: MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
                 )
             }
-            VStack(alignment: .leading, spacing: 4) {
-                Divider()
-                Text("AED List")
-                    .font(.subheadline)
-                    .foregroundStyle(.secondary)
-            }
-            .padding(.horizontal)
-            .padding(.top, 8)
 
             List(aeds) { aed in
                 VStack(alignment: .leading) {
